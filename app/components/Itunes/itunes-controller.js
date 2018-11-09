96ad4aa143b5a8176c38c400f79c3965c4d25b09
alanbutcher <alan.butcher81@gmail.com>
2018-11-09 15:00:29 -0700

    functional but ugly

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -5,11 +5,24 @@ import ItunesService from "./itunes-service.js";
 const itunesService = new ItunesService()
 
 function drawSongs(results) {
-  console.log(results)
+  // console.log(results)
   //YOUR CODING STARTS HERE
-
-
-
+  let template = ''
+  results.forEach(song => {
+    template += `
+    <div class='col card'>
+    <ul>
+      <img src = "${song.albumArt}">
+      <li>${song.title}</li>
+      <li>${song.artist}</li>
+      <li>${song.collection}</li>
+      <a href="https://www.apple.com/itunes/" class="btn btn-outline-success my-2" role="button" aria-pressed ="true" style="margin-right:1vw">$${song.price}</a> 
+      <audio controls controlsList="nodownload" src="${song.preview}"></audio>
+    </ul>
+    </div>
+    `
+  })
+  document.getElementById('song-list').innerHTML = template
 }
 
 
